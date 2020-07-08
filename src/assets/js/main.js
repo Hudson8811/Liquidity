a03eae7f14f80cf37bc9c2aaef173544e5dea561
Hudson8811 <hudson88@yandex.ru>
2020-07-08 18:09:07 +0300

    changed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -15,6 +15,20 @@ setTimeout(function () {
 }, 0);
 
 $(document).ready(function () {
+  $(".fancybox-thumb").fancybox({
+    prevEffect: "none",
+    nextEffect: "none",
+    helpers: {
+      title: {
+        type: "outside",
+      },
+      thumbs: {
+        width: 50,
+        height: 50,
+      },
+    },
+  });
+
   $(
     ".lema_section .hear_flickity--js, .includes_team .hear_flickity--js"
   ).flickity({
