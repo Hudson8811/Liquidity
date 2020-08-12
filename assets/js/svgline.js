fb13ac79a194654781b02a9020ce60659cf3770b
PipZip <zipperpipper@gmail.com>
2020-08-12 18:05:59 +0600

    arrow

--- assets/js/svgline.js
+++ assets/js/svgline.js
@@ -1,16 +1,21 @@
-gsap.registerPlugin(MotionPathPlugin);
-var action = gsap.timeline({ defaults: { ease: "none" } }).
-to(".ball01", { autoAlpha: 1, duration: 0.01 }).
-from(".theGreenLine", { drawSVG: 0, duration: 1 }, 0).
-to(".ball01", { duration: 1,  motionPath: { path: ".theGreenLine", align: ".theGreenLine",  autoRotate: true,  alignOrigin: [0.6, 0.5] } },  0);
+var $doc = $(document),
+    $win = $(window),
+    $svg = $('#svgline').drawsvg(),
+    max = $doc.height() - $win.height();
 
 
-var Controller = new ScrollMagic.Controller();
+$(document).on('scroll', function() {
+    if ($(this).scrollTop() >= $('#svgline').position().top - $win.height()) {
 
-var scene = new ScrollMagic.Scene({
-    triggerElement: "#svgline",
-    duration: 3000,
-    triggerHook: 0.7
-}).
-setTween(action)
-    .addTo(Controller);
+        var p = ($doc.scrollTop()  - $('#svgline').position().top + $win.height()) / $svg.height() *0.85;
+        if (p < 0.282955344441662) p = 0.282955344441662;
+        $svg.drawsvg('progress', p);
+        console.log(p);
+        console.log(p > 0.87);
+        if (p > 0.87){
+            $('.ball01').addClass('active');
+        } else {
+            $('.ball01').removeClass('active');
+        }
+    }
+})
